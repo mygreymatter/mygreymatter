59ed02497cfa1579f0378381e534a8d094380dd2
Mahayogi Lakshmipathi <mahayogi@scimata.com>
2016-02-01 15:46:00 +0530

    Added AngularJS and Bootstrap

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('port', PORT);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-    res.send('Hello, mayo!');
+    res.sendFile(__dirname + '/public/views/index.html');
 });
 
 app.listen(app.get('port'), function () {
